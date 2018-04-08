55edfaa16330550c9f335ac4ffdb73a23709948b
急須 <feedmewithspam@web.de>
2018-04-08 10:44:39 +0200

    Sketched out basic command line functionality

---
+++ getIframe.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const UrlAssembler = require('url-assembler');
+const getStdin = require('get-stdin');
+const {fromPromise, fromNode, Rejected, Resolved, all} = require('crocks/Async');
+const {split, compose, reject, isEmpty, map, join} = require('ramda');
+
+const getFileContentAsBuffer = fromNode(fs.readFile);
+
+const bufferToBase64 = buffer => buffer.toString('base64');
+
+const getRunkitURL = fileName => base64Code => UrlAssembler('https://runkit.com')
+    .prefix('e')
+    .query({
+        name: fileName,
+        gutterStyle: 'outside',
+        base64source: base64Code
+    })
+    .toString();
+
+const getIframe = fileName => url => `<iframe data-file-name="${fileName}" src="${url}"></iframe>`;
+
+const convertFileToRunkitIframe = fileName => getFileContentAsBuffer(fileName)
+    .chain(checkInput(`"${fileName}" is empty!`))
+    .map(bufferToBase64)
+    .map(getRunkitURL(fileName))
+    .map(getIframe(fileName));
+
+const getStdinAsync = fromPromise(getStdin);
+
+const checkInput = errorMessage => input => input && input.length ? Resolved(input) : Rejected(errorMessage);
+
+const getLines = compose(reject(isEmpty), split('\n'));
+
+const getIframes = compose(all, map(convertFileToRunkitIframe));
+
+module.exports = getStdinAsync()
+    .chain(checkInput('No input was given!'))
+    .map(getLines)
+    .chain(getIframes)
+    .map(join('\n'));
--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-const fs = require('fs');
-const Async = require('crocks/Async');
-const UrlAssembler = require('url-assembler');
-
-const {fromNode} = Async;
-
-const getFileContentAsBuffer = fromNode(fs.readFile);
-
-const bufferToBase64 = buffer => buffer.toString('base64');
-
-const getRunkitURL = fileName => base64Code => UrlAssembler('https://runkit.com')
-    .prefix('e')
-    .query({
-        name: fileName,
-        gutterStyle: 'outside',
-        base64source: base64Code
-    })
-    .toString();
-
-const getIframe = fileName => url => `<iframe data-file-name="${fileName}" src="${url}"></iframe>`;
-
-
-const convertFileToRunkitIframe = fileName => getFileContentAsBuffer(fileName)
-    .map(bufferToBase64)
-    .map(getRunkitURL(fileName))
-    .map(getIframe(fileName))
-    .fork(console.log, console.log);
-
-module.exports = convertFileToRunkitIframe;
+const getIframes = require('./getIframe');
+/*eslint fp/no-unused-expression: 0*/
+getIframes.fork(console.error, console.log);
