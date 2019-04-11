55f5d9143a839766847ffcc5e631ab1476d28f09
急須 <feedmewithspam@web.de>
2019-04-11 15:49:52 +0200

    Add end to end tests

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 const getIframes = require('./getIframe');
+/*eslint better/explicit-return: 0*/
+/*eslint fp/no-nil: 0*/
+const errFn = err => {
+    console.error(err);
+    process.exit(1);
+};
 /*eslint fp/no-unused-expression: 0*/
-getIframes.fork(console.error, console.log);
+getIframes.fork(errFn, console.log);
